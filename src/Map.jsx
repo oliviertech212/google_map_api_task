619b701fbd5fb7e711a8599954e0d95714f92621
oliviertech212 <oliviertech212@gmail.com>
2024-04-28 17:45:03 +0200

    change stops

--- src/Map.jsx
+++ src/Map.jsx
@@ -23,14 +23,21 @@ const Map = () => {
   const [timeToNextStop, setTimeToNextStop] = useState(null);
 
   const [nextStopName, setNextStopName] = useState("");
+  // const stops = [
+  //   { lat: -1.939826787816454, lng: 30.0445426438232 }, // Starting Point: Nyabugogo
+  //   { lat: -1.9355377074007851, lng: 30.060163829002217 }, // Stop A
+  //   { lat: -1.9358808342336546, lng: 30.08024820994666 }, // Stop B
+  //   { lat: -1.9489196023037583, lng: 30.092607828989397 }, // Stop C
+  //   { lat: -1.9592132952818164, lng: 30.106684061788073 }, // Stop D
+  //   { lat: -1.9487480402200394, lng: 30.126596781356923 }, // Stop E
+  //   { lat: -1.9365670876910166, lng: 30.13020167024439 }, // Ending Point: Kimironko
+  // ];
+
   const stops = [
-    { lat: -1.939826787816454, lng: 30.0445426438232 }, // Starting Point: Nyabugogo
-    { lat: -1.9355377074007851, lng: 30.060163829002217 }, // Stop A
-    { lat: -1.9358808342336546, lng: 30.08024820994666 }, // Stop B
-    { lat: -1.9489196023037583, lng: 30.092607828989397 }, // Stop C
-    { lat: -1.9592132952818164, lng: 30.106684061788073 }, // Stop D
-    { lat: -1.9487480402200394, lng: 30.126596781356923 }, // Stop E
-    { lat: -1.9365670876910166, lng: 30.13020167024439 }, // Ending Point: Kimironko
+    { lat: -1.9675632902588955, lng: 30.06715805248569 },
+    { lat: -1.963570990711944, lng: 30.060070967805924 },
+    { lat: -1.9646810122888905, lng: 30.062852408309627 },
+    { lat: -1.9566332658144245, lng: 30.064215796977617 },
   ];
   const averageSpeed = 40; // average speed is 40 km/h
   const directionsService = useRef(null);
@@ -160,6 +167,7 @@ const Map = () => {
           </div>
 
           <div className="my-3"></div>
+          <div>ETA to next stop: {calculateETA()} </div>
         </div>
       )}
       <div className="  bg-gradient-to-r from-cyan-300 to-green-500 hidden md:block relative w-full md:w-1/4 border rounded-md p-5 shadow-md md:bg-white">
